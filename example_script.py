110c6cde59b7cbb5be6f79e79660b0c02a7b8b6d
jamiewoych <jw3943@columbia.edu>
2025-02-12 22:46:28 -0500

    tutorial 7.3 hw

---
+++ example_script.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+A description of what this script contains.
+"""
+
+import builtins
+
+# this is where the module-type code goes
+class Example:
+    "This is where classes should go"
+    def __init__(self):
+        pass
+
+def example_func():
+    "This is where stand-alone functions should go"
+    pass
+
+# this is where executable-type code goes, this code is not 
+# executed if this script is only imported.
+if __name__ == "__main__":
+    print("this code is not run when this file is imported.")
